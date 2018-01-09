9915ad8f0978893eda6a7100a749f0584f8b7c9e
Daniel Rico <dericop@unal.edu.co>
2018-01-09 16:33:25 -0500

    Inicio detalle de foro

---
+++ app/static/app/logic/controllers/forumDetailCtrl.js
@@ -0,0 +1,15 @@
+(function(){
+	angular
+		.module('app.controllers')
+		.controller('forumDetailController', forumDetailController)
+
+	forumDetailController.$inject = ["$scope"];
+
+	function forumDetailController($scope){
+		var that = this;
+
+		
+	}
+
+
+})();
