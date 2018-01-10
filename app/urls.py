6d5de0ba2666e75b5530367ae5aa149f1dd170aa
Luis Daniel <luisdaniel.ld24@gmail.com>
2018-01-10 07:51:51 -0500

    Change password

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,10 @@ from .views import Index
 from .views import Login
 from .views import UsernameCheck
 from .views import Forums
+from .views import PasswordReset
+from .views import PasswordResetDone
+from .views import PasswordResetConfirm
+from .views import PasswordResetComplete
 
 urlpatterns = [
 	path('', Index.as_view(), name='index'),
@@ -16,5 +20,9 @@ urlpatterns = [
 	path('logout', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 	path('api/users/<str:user_id>', UsernameCheck.as_view(), name='username_check'),
 	path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),
+	path('password_reset_request/', PasswordReset.as_view(), name='password_reset_request'),
+	path('password_reset_request_done/', PasswordResetDone.as_view(), name='password_reset_done'),
+	path('password_reset_confirm/<uidb64>/<token>', PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+	path('password_reset_complete/', PasswordResetComplete.as_view(), name='password_reset_complete'),
 	path('forums', Forums.as_view(), name='forums')
 ]
