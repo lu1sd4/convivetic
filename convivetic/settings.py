0aaeb949bfe3a41185da456cccc21f7df1f45636
Luis Daniel <luisdaniel.ld24@gmail.com>
2018-02-18 17:19:27 -0500

    Uploadiloading videos and audios to ConviveTIC account

--- convivetic/settings.py
+++ convivetic/settings.py
@@ -128,9 +128,9 @@ CLIENT_ID = '367931440425-7d9ukeoek3s1pacp66f4ou9vvgds0lk9.apps.googleuserconten
 CLIENT_SECRET = 'pUC0H5AH9S7oIjCpPOXUBmcf'
 SCOPE = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.metadata https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.photos.readonly https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.scripts https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.upload'
 API_KEY = 'AIzaSyA6hCsmPkcjbcWNHqrXAR-7KHHwN92ONLk'
-DRIVE_FOLDER_ID = '1PXtw_L0urVh8VdToKUthWT-FiR4ZsKZl'
+DRIVE_FOLDER_ID = '1lBJJTL73djrHXaSssIr-3gHE9QPkJ96Q'
 
 # User creds
 
-REFRESH_TOKEN = '1/fOJ7MVeJzeYNbym_6Qwsrkh5Fim2LtXIdtVBLwyKjy4'
-ACCESS_TOKEN = 'ya29.GlxABaTlnqWLvABrhPqNv6UtAMcAoKd2lp0IZeUxTcGup29kRA59WUxlmSXOew4L6nxkUD90-10wd1Xunvdn79pnk8jrwtSkLeHX1VBsIDheXoRFVeUulbfXL4HAmQ'
+REFRESH_TOKEN = '1/-MIfoXnTYScaGGfjNDqS2I0YTsx_jNB0hLpbxzGUxXg'
+ACCESS_TOKEN = 'ya29.GltmBWhETDQ9npXjZnXG9c7gj7fNHKeE9DU_pIH-Cvad-_41rs1dwbF-G9XCElmQdqqhw6gE6x8EJGRrmm1ZNCZViIstp4_wt_DIDyFlAy0duaZq-4nYv-UIBWMN'
